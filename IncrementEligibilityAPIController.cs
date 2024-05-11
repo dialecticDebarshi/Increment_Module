5559149231f17acc5f11409f10441ddefc65f524
dialecticDebarshi <140203504+dialecticDebarshi@users.noreply.github.com>
2024-05-11 17:12:10 +0530

    Add files via upload

---
+++ IncrementEligibilityAPIController.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using StaffType.Api.Interface;
+using StaffType.Api.Models;
+using TenantCompany.Models;
+
+namespace StaffType.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class IncrementEligibilityAPIController : Controller
+    {
+        private IIncrementEligibility iIncrementEligibility;
+        public IncrementEligibilityAPIController(IIncrementEligibility incrementEligibilityrepo)
+        {
+            iIncrementEligibility = incrementEligibilityrepo;
+        }
+
+
+
+        [HttpGet]
+        [Route("GetIncrementEligibility")]
+        public IEnumerable<IncrementEligibilityAPIModel> GetIncrementEligibility()
+        {
+            return iIncrementEligibility.GetIncrementEligibility();
+        }
+
+        [HttpGet]
+        [Route("GetTableForm")]
+        public List<SelectListItem> GetTableForm()
+        {
+            List<SelectListItem> dt = iIncrementEligibility.GetTableForm();
+            return dt;
+
+        }
+        [HttpGet]
+        [Route("GetFieldForm/{ApplicationDataTable_Master_KEY}")]
+        public string GetFieldForm(int ApplicationDataTable_Master_KEY)
+        {
+            return iIncrementEligibility.GetFieldForm(ApplicationDataTable_Master_KEY);
+
+
+        }
+
+        [HttpPost]
+        [Route("SaveIncrementEligibility")]
+        public int SaveIncrementEligibility(IncrementEligibilityAPIModel model)
+        {
+            if (model.MAST_INCREMENT_ELIGIBILITY_KEY == 0)
+            {
+                return iIncrementEligibility.SaveIncrementEligibility(model, "INSERT");
+            }
+            else 
+            
+            {
+                return iIncrementEligibility.SaveIncrementEligibility(model, "UPDATE");
+            }
+            //return iIncrementEligibility.SaveIncrementEligibility(model);
+        }
+
+
+
+        [HttpGet]
+        [Route("EditIncrementEligibility/{id}")]
+        public IEnumerable<IncrementEligibilityAPIModel> EditIncrementEligibility(int id)
+        {
+            return iIncrementEligibility.GetIncrementEligibility(id);
+
+        }
+
+        [HttpGet]
+        [Route("DeleteIncrementEligibility/{id}")]
+        public int DeleteIncrementEligibility(int id)
+        {
+            IncrementEligibilityAPIModel Model = new IncrementEligibilityAPIModel();
+            // MIUI.REC_TYPE = "DELETE";
+
+            Model.MAST_INCREMENT_ELIGIBILITY_KEY = id;
+
+            int r = iIncrementEligibility.DeleteIncrementEligibility(Model,"DELETE");
+            return r;
+
+        }
+
+
+    }
+}
+
---
+++ IncrementSetupAPIController.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using StaffType.Api.Interface;
+using StaffType.Api.Models;
+
+namespace StaffType.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class IncrementSetupAPIController : ControllerBase
+    {
+        I_IncrementSetup I_IncrementSetup;
+        public IncrementSetupAPIController(I_IncrementSetup i_IncrementSetup)
+        {
+            I_IncrementSetup = i_IncrementSetup;
+        }
+        [HttpGet]
+        [Route("GetCompany")]
+        public List<object> GetCompany()
+        {
+           return I_IncrementSetup.GetCompanyNames();
+        }
+        [HttpPost]
+        [Route("GetCostCenterList")]
+        public string GetCostCenterList(IncrementSetupAPIModel model)
+        {
+            return I_IncrementSetup.GetCostCenterList(model.Companykey);
+        }
+        [HttpPost]
+        [Route("GetDivisionList")]
+        public string GetDivisionList(IncrementSetupAPIModel model)
+        {
+            return I_IncrementSetup.GetDivisionList(model.Costcenterkey);
+        }
+        [HttpPost]
+        [Route("GetWarehouseList")]
+        public string GetWarehouseList(IncrementSetupAPIModel model)
+        {
+            return I_IncrementSetup.GetWarehouseList(model.DivisionKey, model.Companykey);
+        }
+        [HttpPost]
+        [Route("GetDesignations")]
+        public string GetDesignations(IncrementSetupAPIModel model)
+        {
+            return I_IncrementSetup.GetDesignationsCostcenter_Division_Warehouse(model.Companykey, model.Costcenterkey, model. DivisionKey, model.WarehouseKey,Convert.ToInt32(model.SetupKey));
+        }
+
+        [HttpGet]
+        [Route("GetApplicableComponent")]
+        public List<object> GetApplicableComponent()
+        {
+            return I_IncrementSetup.GetApplicableComponent();
+        }
+
+        [HttpGet]
+        [Route("GetEligibility")]
+        public string GetEligibility()
+        {
+            return I_IncrementSetup.LoadEligibility();
+        }
+        [HttpPost]
+        [Route("SaveIncrementSetup")]
+        public int SaveIncrementSetup(List<IncrementSetupAPIModel> model)
+        {
+            return I_IncrementSetup.SaveincrementSetup(model);
+
+        }
+        [HttpGet]
+    
+        public List<IncrementSetupAPIModel> Get()
+        {
+            var r = I_IncrementSetup.getallincrementsetup();
+            return r;
+        }
+        [HttpGet]
+        [Route("GetByid/{INCREMENT_SETUP_KEY}")]
+        public List<IncrementSetupAPIModel> GetByid(int INCREMENT_SETUP_KEY)
+        {
+            var r = I_IncrementSetup.getallincrementsetupByid(INCREMENT_SETUP_KEY);
+            return r;
+        }
+        [HttpPost]
+        [Route("DeleteSetup/{id}")]
+        public int DeleteSetup(int id)
+        {
+
+            return I_IncrementSetup.Deletesetup(id);
+        }
+
+    }
+}
